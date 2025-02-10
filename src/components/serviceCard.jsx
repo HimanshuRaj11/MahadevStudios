d20f291b34e84bd086f13dc6ebc67a057e3af580
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-02-11 00:47:10 +0530

    commit

--- src/components/serviceCard.jsx
+++ src/components/serviceCard.jsx
@@ -31,9 +31,9 @@ const services = [
 
 export default function Services() {
     return (
-        <div className="flex gap-6 justify-center backdrop-blur-sm p-10 flex-wrap">
+        <div className="flex gap-6 justify-center  p-10 flex-wrap">
             {services.map((service, index) => (
-                <Card key={index} className="bg-[#231815] max-w-72 text-white p-6 cursor-pointer rounded-xl border border-gray-700 hover:shadow-lg  hover:bg-[#2e1f1a] hover:scale-105 transform transition-transform duration-300">
+                <Card key={index} className="bg-[#231815] backdrop-blur-sm max-w-72 text-white p-6 cursor-pointer rounded-xl border border-gray-700 hover:shadow-lg  hover:bg-[#2e1f1a] hover:scale-105 transform transition-transform duration-300">
                     <CardContent className="flex flex-col items-start gap-4">
                         <div className="p-3 bg-yellow-900/20 rounded-full">{service.icon}</div>
                         <h3 className="text-lg font-semibold">{service.title}</h3>
