79d7b7f6356506cd05adeea430e6aa0a97bf4ea1
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-02-13 22:09:05 +0530

    commit

--- src/app/our-work/page.jsx
+++ src/app/our-work/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function page() {
     return (
         <div className='min-h-screen w-[100%] pb-4'>
-            <div className="h-[60vh] bg-cover bg-[url('/octdes10.jpg')] flex justify-center items-center">
+            <div className="h-[60vh] bg-cover bg-[url('/octdes10.jpg')] flex justify-center items-center" style={{ boxShadow: 'inset 0 -200px 100px -100px black' }}>
                 <h1 className=' text-3xl md:text-8xl p-5 backdrop-blur-md rounded-md text-white font-bold border'>Our Event</h1>
             </div>
             <div className="flex flex-wrap justify-center items-center py-10">
