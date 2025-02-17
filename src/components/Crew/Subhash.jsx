2c1c54502f9daf475a399d46750e68790ce513e2
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-02-17 17:51:05 +0530

    commit

--- src/components/Crew/Subhash.jsx
+++ src/components/Crew/Subhash.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export default function Subhash() {
     return (
-        <div>
-
+        <div className="p-5 leading-relaxed font-sans text-lg text-white ">
+            <h1 className="text-white text-2xl font-bold">Hello, I'm Subhash Prince</h1>
+            <p className="text-xl">
+                Since childhood, I have been passionate about singing. Currently, I am pursuing a degree in classical music,
+                which has been a lifelong dream of mine. Under the guidance of my esteemed Guru, Shri Kamlesh Mukul ji,
+                I have honed my skills in classical music.
+            </p>
+            <p className="text-xl">
+                My interests span across various genres including classical singing, film music, and folk music.
+                I find immense joy in performing songs from these genres.
+            </p>
+            <p className="text-xl font-bold">
+                Music is not just a hobby for me; it is my life's passion. There is nothing else that captivates me as much as music does.
+            </p>
         </div>
-    )
+    );
 }
--- src/components/CrewCard.jsx
+++ src/components/CrewCard.jsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import React from 'react';
-import { FaPhone, FaMapMarkerAlt, FaInstagram } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
+
 export default function CrewCard({ crew }) {
     return (
         <div className="w-[18rem] h-[18rem] rounded-lg flex flex-col justify-around items-center backdrop-blur-sm border  hover:scale-105 transform transition-transform duration-300 hover:shadow-md cursor-pointer">
@@ -7,8 +9,10 @@ export default function CrewCard({ crew }) {
                 <img src={crew.image} className=' aspect-square rounded-full w-[40%] object-center object-cover' alt="" />
                 <h1 className='font-semibold text-3xl text-white'>{crew.name}</h1>
                 <h1 className='font-semibold text-xl text-white'>{crew.cast} </h1>
+                <Link href={{ pathname: `/${crew.id}`, query: { id: `${crew.id}` } }} className='bg-gray-800 rounded-md border text-white p-2 m-2 flex items-center'>
+                    View More <FaArrowRight className="ml-2" />
+                </Link>
             </div>
-
         </div>
     );
 }
